1e6a9429e5a235922845085c29741fce60206cc3
Madan-ghodechor <madanghodechor6232@gmail.com>
2023-10-04 12:02:17 +0530

    btns changed

--- src/app/my-profile/my-profile.component.ts
+++ src/app/my-profile/my-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-profile',
@@ -8,12 +9,20 @@ import { ApiService } from '../api.service';
 })
 export class MyProfileComponent {
 
+  token:any;
   userData:any;
-  constructor(private api:ApiService){
+  constructor(private api:ApiService, private router:Router){
     this.api.getUserProfile().subscribe((res:any)=>{
       console.log(res);
       this.userData = res.data[0];
       console.log(this.userData)
+      this.token = localStorage.getItem('token');
     })
+  }
+  userLogOut()
+  {
+    localStorage.clear();
+    this.token = localStorage.getItem('token');
+    this.router.navigate(['/login'])
   }
 }
